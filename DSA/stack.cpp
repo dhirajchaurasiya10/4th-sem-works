f276d5ecb2c34708d29cc99c2a2e44835c9bf2d1
dhirajchaurasiya10 <starkjon224@gmail.com>
2023-12-23 23:35:07 +0545

    updated

--- DSA/stack.cpp
+++ DSA/stack.cpp
@@ -1,6 +1,48 @@
 #include<iostream>
 #include<cmath>
+#define max 5
 using namespace std;
-int main(){
 
+int arr[max];
+int first = -1;
+
+void push(int data){
+	int i;
+	first+=1;
+	cout<<"push: ";
+	for(i=first;i>0;i--){
+		arr[i]=arr[i-1];
+		arr[0]=data;
+		cout<<arr[0]<<endl;
+	}
+}
+
+int pop(){
+	int i,value;
+	value=arr[0];
+	for(i=0;i<first;i++){
+		arr[0]=arr[i+1];
+		first-=1;
+	}
+	return value;
+}
+
+void print()
+	{
+		int i;
+		if(first=-1){
+			cout<<"stack underflow"<<endl;
+		}
+		else {
+			cout<<"ater pop"<<endl;
+			for(i=0;i<=first;i++){
+				cout<<arr[i]<<endl;
+			}
+		}
+	}
+int main(){
+//	int data;
+	push(3);
+	push(9);
+//	data = pop();
 }
