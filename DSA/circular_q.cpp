8489be1c88466e674f5ab8a3aab913c01a7c9c19
dhirajchaurasiya10 <starkjon224@gmail.com>
2024-03-07 23:07:28 +0545

    updated

--- DSA/circular_q.cpp
+++ DSA/circular_q.cpp
@@ -8,45 +8,48 @@ int rear=-1;
 int item[max];
 
 void enq(int x){
-	if(front==((rear+1)%max)){
-		cout<<"queue is overflow"<<endl;
-	}
-	else if(front==-1 || rear==-1){
-			front=0;
-			rear=0;
-		}
-	else {
-			item[rear]=x;
-			rear=(rear+1)%max;
-			cout<<x<<" added to queue"<<endl;
-		}
+    if(front==((rear+1)%max)){
+        cout<<"queue is overflow"<<endl;
+    }
+    else if(front==-1 || rear==-1){
+            front=0;
+            rear=0;
+        }
+    else {
+            item[rear]=x;
+            rear=(rear+1)%max;
+            cout<<x<<" added to queue"<<endl;
+        }
 }
 
 void deq() {
-	if(front==-1){
-		cout<<"queue is underflow"<<endl;
-	}
-	else if(front==rear){
-		front=-1;
-		rear=-1;		
-	}
-	else {
-		front=(front+1)%max;
-	}
+    if(front==-1){
+        cout<<"queue is underflow"<<endl;
+    }
+    else if(front==rear){
+        front=-1;
+        rear=-1;        
+    }
+    else {
+        front=(front+1)%max;
+    }
 }
 
 void display(){
-	cout<<"displaying queue"<<endl;
-	for(int i=front;i<=((rear+1)%max);i++){
-		cout<<item[i]<<endl;
-	}
+    cout<<"displaying queue"<<endl;
+    int i = front;
+    while (i != rear) {
+        cout<<item[i]<<endl;
+        i = (i + 1) % max;
+    }
 }
 
 int main(){
-	enq(5);
-	enq(3);
-	enq(4);
-	enq(9);
-	display();
-	
+    enq(5);
+    enq(3);
+    enq(4);
+    enq(9);
+    display();
+    deq();
+    display();
 }
